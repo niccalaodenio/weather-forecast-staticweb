b8a580f37a5314d40f1bc4824dce351605cd12d4
NICCA <niccalaodenio26@gmail.com>
2022-08-23 00:14:01 +0800

    new apikey

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,18 +1,18 @@
-const API_key = 'G8xT2HdX2cbBHa8QRWWW41ax2S9irS73'
+const API_key = "G8xT2HdX2cbBHa8QRWWW41ax2S9irS73";
 
 // getting city info
-let getCity = async (city)=>{
-    try{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${API_key}&q=${city}`
-    const res = await fetch(base + query)
-    const data = await res.json()
-    return data[0] // to return the first output
-    }catch(e){
-        console.log('something went wrong', e)
-    }
-
-}
+let getCity = async (city) => {
+  try {
+    const base =
+      "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${API_key}&q=${city}`;
+    const res = await fetch(base + query);
+    const data = await res.json();
+    return data[0]; // to return the first output
+  } catch (e) {
+    console.log("something went wrong", e);
+  }
+};
 // getCity('Mapanas')
 //     .then(data =>{
 //      //console.log( data)
@@ -20,16 +20,16 @@ let getCity = async (city)=>{
 //     }).then(data => data);
 
 //getting current weather condition
-let getWeather = async (id)=>{
-    try{
-        let host = 'http://dataservice.accuweather.com/currentconditions/v1/'
-        let query = `${id}?apikey=${API_key}`
-        let res = await fetch(host + query)
-        var data = await res.json();
-        //console.log(data)
-        return data[0]
-    }catch(err){
-        console.log(err)
-    }
-}
+let getWeather = async (id) => {
+  try {
+    let host = "http://dataservice.accuweather.com/currentconditions/v1/";
+    let query = `${id}?apikey=${API_key}`;
+    let res = await fetch(host + query);
+    var data = await res.json();
+    //console.log(data)
+    return data[0];
+  } catch (err) {
+    console.log(err);
+  }
+};
 //getWeather('265075')
